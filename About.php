e1ad7b801d2e3c1633619e06838d7d50129b60b1
Roel Wolters <r.wolters@student.avans.nl>
2017-02-07 12:50:09 +0100

    Opmaak via CSS verbeterd

--- About.php
+++ About.php
@@ -1,4 +1,3 @@
-<?php>
 <html>
 	<script src="http://www.w3schools.com/lib/w3data.js"></script>
 	<head>
@@ -7,9 +6,10 @@
 	<body>
 		<div w3-include-html="header.php"></div>
 		<script>w3IncludeHTML();</script>
+		<link rel="stylesheet" href="css/AboutStyle.css"/>
 
 		<h1>Over deze winkel</h1>
-		<p>Dit is de beste winkel van de wereld. Deze winkel is zo vreselijk goed dat er geen nuttige informatie in de About hoeft te staan. Koop hier al je boeken, of anders zullen we je vinden en je ophangen aan je ingewanden.</p>
+		<p>Dit is de beste winkel van de wereld. Deze winkel is zo vreselijk goed dat er geen nuttige informatie in de Over Ons hoeft te staan. Koop hier al je boeken, of anders zullen we je vinden en je ophangen aan je ingewanden.</p>
 		<p><b>Contactadres:</b> Achter je!</p>
 		<p><b>KOOP HIER AL JE BOEKEN</b></p>
 		<p><b>KOOP HIER AL JE BOEKEN</b></p>
--- Home.php
+++ Home.php
@@ -1,4 +1,3 @@
-<?php>
 <html>
 	<script src="http://www.w3schools.com/lib/w3data.js"></script>
 	<head>
@@ -7,18 +6,33 @@
 	<body>
 		<div w3-include-html="header.php"></div>
 		<script>w3IncludeHTML();</script>
-
+		<link rel="stylesheet" href="css/HomeStyle.css"/>
+		
 		<h1>Aanbiedingen bij boekenwinkel</h1>
 		
-		<div>
-			<p><img src="https://www.fredsbouwtekeningen.nl/wp-content/uploads/2014/09/Boekenkast-maken.png" height="350" width="233"></p>
+		<div class="product prleft">
+			<p class="productimg"><img src="https://www.fredsbouwtekeningen.nl/wp-content/uploads/2014/09/Boekenkast-maken.png" height="350" width="192"></p>
 			<h2>Alle boeken die we hebben</h2>
 			<h3>€0,99</h3>
 			<p>Lorem ipsum dolor sit amet blah blah blah (omschrijving)</p>
 		</div>
 		
-		<div>
-			<p><img src="http://www.politicalgarbagechute.com/wp-content/uploads/2014/04/magic-book-burning-247.jpg" height="120" width="192"></p>
+		<div class="product prright">
+			<p class="productimg"><img src="http://www.politicalgarbagechute.com/wp-content/uploads/2014/04/magic-book-burning-247.jpg" height="120" width="192"></p>
+			<h2>Brandwonden</h2>
+			<h3>€0,99</h3>
+			<p>Lorem ipsum dolor sit amet blah blah blah (omschrijving)</p>
+		</div>
+		
+		<div class="product prleft">
+			<p class="productimg"><img src="https://www.fredsbouwtekeningen.nl/wp-content/uploads/2014/09/Boekenkast-maken.png" height="350" width="192"></p>
+			<h2>Alle boeken die we hebben</h2>
+			<h3>€0,99</h3>
+			<p>Lorem ipsum dolor sit amet blah blah blah (omschrijving)</p>
+		</div>
+		
+		<div class="product prright">
+			<p class="productimg"><img src="http://www.politicalgarbagechute.com/wp-content/uploads/2014/04/magic-book-burning-247.jpg" height="120" width="192"></p>
 			<h2>Brandwonden</h2>
 			<h3>€0,99</h3>
 			<p>Lorem ipsum dolor sit amet blah blah blah (omschrijving)</p>
