23d3282a1b011c6118d6b17bc0e430098d74e546
Rick Wouters <rawouter@student.avans.nl>
2017-02-15 16:43:58 +0100

    database class finished

--- ClassFiles/database.inc.php
+++ ClassFiles/database.inc.php
@@ -1,1 +1,17 @@
 <?php
+
+
+	function sendQuery($query){
+		
+		$link = mysqli_connect("localhost", "root", "") or die ("something went wrong");
+		$db = mysqli_select_db($link, "bookstoredb");
+		
+		$result = mysqli_query($link, $query);
+		
+		mysqli_close($link);
+		
+		return $result;
+		
+	}
+	
+?>
